21736c545b67fddb6a2d9174b04e3232b0d69d7a
adiyathereh <ad.kydy@gmail.com>
2018-09-17 15:27:45 +0200

    created column schema

---
+++ src/server/models/Column.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const autopopulate = require("mongoose-autopopulate");
+
+const columnSchema = new Schema({
+  title: String,
+  limit: Number,
+  order: Number,
+  ticket: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Ticket",
+      autopopulate: true
+    }
+  ]
+});
+
+columnSchema.plugin(autopopulate);
+
+module.exports = mongoose.model("Column", columnSchema);
