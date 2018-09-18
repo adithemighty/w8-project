11dda3201657ac822a11b0f4e7ddc68eb1e40d4a
adiyathereh <ad.kydy@gmail.com>
2018-09-18 14:40:21 +0200

    implemented reordering inside of one list

--- src/app/js/Board.jsx
+++ src/app/js/Board.jsx
@@ -15,29 +15,60 @@ class Board extends Component {
       this.setState(function(prevState, props) {
         const newState = {
           title: "",
-          columns: []
+          columns: {}
         };
         const { title, columns } = data[0];
         newState.title = title;
 
-        newState.columns = columns.map(({ title, ticket, _id }) => {
-          return {
-            title,
-            ticket,
-            _id
+        columns.forEach(({ title, ticket, _id }) => {
+          newState.columns[title] = {
+            tickets: ticket,
+            id: _id,
+            title: title
           };
         });
-
         return { ...newState };
       });
     });
   }
 
+  reorder = (listName, list, oldPos, newPos) => {
+    var movingElement = list[oldPos];
+    //delete element from old position
+    list.splice(oldPos, 1);
+    //add copy of element to array in the new position
+    list.splice(newPos, 0, movingElement);
+    this.setState(function(prevState, props) {
+      const newState = prevState;
+      newState.columns[listName].tickets = list;
+      return newState;
+    });
+  };
+
   onDragEnd = result => {
-    console.log("columns", this.state.columns);
-    console.log("source", result.source.droppableId);
-    console.log("destination", result.destination.droppableId);
+    const startColumn = result.source.droppableId;
+    const endColumn = result.destination.droppableId;
+    const initIndex = result.source.index;
+    const endIndex = result.destination.index;
+
     console.log("result", result);
+    //DnD inside of one column
+    if (startColumn === endColumn) {
+      //1. find column in state
+      //   console.log("movement inside of one column");
+      //   console.log(
+      //     `movement from position ${initIndex} to ${endIndex} inside ${
+      //       this.state.columns[startColumn].title
+      //     }`
+      //   );
+      //2. define new index for column
+      this.reorder(
+        startColumn,
+        this.state.columns[startColumn].tickets,
+        initIndex,
+        endIndex
+      );
+    }
   };
 
   render() {
@@ -45,18 +76,20 @@ class Board extends Component {
       console.log("it's null");
       return <div>Loading</div>;
     } else {
+      console.log(this.state);
       return (
         <div>
           {this.state.title}
 
           <DragDropContext onDragEnd={this.onDragEnd}>
-            {this.state.columns.map(column => {
+            {Object.keys(this.state.columns).map(columnName => {
+              const column = this.state.columns;
               return (
                 <Column
-                  key={column.title}
-                  title={column.title}
-                  id={column._id}
-                  tickets={column.ticket}
+                  key={column[columnName].title}
+                  title={column[columnName].title}
+                  id={column[columnName].id}
+                  tickets={column[columnName].tickets}
                 />
               );
             })}
