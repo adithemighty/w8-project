eeb8f21f249a1c184bf99aee309b5f45be1b243f
adiyathereh <ad.kydy@gmail.com>
2018-09-20 17:14:56 +0200

    added fields to ticket schema

--- src/server/models/Ticket.js
+++ src/server/models/Ticket.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 const ticketSchema = new Schema({
   title: String,
-  blocker: { type: Boolean, default: false }
+  blocker: { type: Boolean, default: false },
+  description: String,
+  estimation: String,
+  key: String
 });
 
 module.exports = mongoose.model("Ticket", ticketSchema);
