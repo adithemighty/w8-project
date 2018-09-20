8591955d43abf65147d23c643062b3bfaecf86b6
adiyathereh <ad.kydy@gmail.com>
2018-09-20 11:47:31 +0200

    added limit to props passed from board to column

--- src/app/js/Column/index.jsx
+++ src/app/js/Column/index.jsx
@@ -5,6 +5,7 @@ import { Droppable } from "react-beautiful-dnd";
 
 const Column = props => {
   const { title, tickets, id, boardId, columns } = props;
+  console.log("props", props);
 
   const ticketsList = tickets.map((ticket, ind) => {
     return <Card key={ind} index={ind} ticket={ticket} />;
