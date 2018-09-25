9afaea68eb3cc92ac2be8038ba66e11e6c1f4fbc
adiyathereh <ad.kydy@gmail.com>
2018-09-25 19:20:39 +0200

    columns have wip limit field that is somewhat styled

--- src/app/js/Column/index.jsx
+++ src/app/js/Column/index.jsx
@@ -1,74 +1,109 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "../Card";
 import DeleteColumnButton from "./DeleteColumnButton";
 import ArrowLeft from "../../assets/arrLeft.svg";
 import ArrowRight from "../../assets/arrRight.svg";
 import { Droppable } from "react-beautiful-dnd";
+import { RIEInput, RIETextArea, RIESelect } from "riek";
 
-const Column = props => {
-  const { title, tickets, id, boardId, columns } = props;
+class Column extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      limit: ""
+    };
+  }
 
-  const ticketsList = tickets.map((ticket, ind) => {
-    return (
-      <Card
-        ticketDetailViewOpenHandler={props.ticketDetailViewOpenHandler}
-        key={ind}
-        index={ind}
-        ticket={ticket}
-      />
-    );
-  });
+  handleInputChange = field => {
+    console.log(field);
+    this.setState((prevState, props) => {
+      return field;
+    });
+  };
 
-  return (
-    <div className="column">
-      {/* column header with title and delete button */}
-      <div className="column-header">
-        {props.first ? null : (
-          <button
-            onClick={() => props.columnMoveHandler("left", props.ind)}
-            className="icon-button"
-          >
-            <img className="icon" src={ArrowLeft} alt="" />
-          </button>
-        )}
-        <p className="column-title">{title}</p>
-        <p>{props.limit ? `${props.tickets.length}/${props.limit}` : null}</p>
+  render() {
+    const { title, tickets, id, boardId, columns } = this.props;
 
-        <DeleteColumnButton
-          sourceColumnId={id}
-          columnHasTickets={tickets.length > 0 ? true : false}
-          getBoardData={props.getBoardData}
-          boardId={boardId}
-          columns={columns}
+    const ticketsList = tickets.map((ticket, ind) => {
+      return (
+        <Card
+          ticketDetailViewOpenHandler={this.props.ticketDetailViewOpenHandler}
+          key={ind}
+          index={ind}
+          ticket={ticket}
         />
-        {props.last ? null : (
-          <button
-            onClick={() => props.columnMoveHandler("right", props.ind)}
-            className="icon-button"
-          >
-            <img className="icon" src={ArrowRight} alt="" />
-          </button>
-        )}
-      </div>
+      );
+    });
 
-      {/* this is necessary for the drag and drop to work so that after dnd we know which is the source and destination column. KEY IS IMPORTANT! DON'T DELETE, MOVE OR WHATEVER! OTHERWISE DRAG AND FUCKING DROP WILL NOT FUCKING MOVE! */}
-      <Droppable key={id} droppableId={title}>
-        {(provided, snapshot) => (
-          <div
-            className="column-container"
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <div className="column-tickets">
-              {ticketsList}
-              {/* the placeholder is there so that the space from which a card is dragged  doesn't collapse */}
-              {provided.placeholder}
-            </div>
+    return (
+      <div className="column">
+        {/* column header with title and delete button */}
+        <div className="column-header">
+          <div className="limit">
+            <label htmlFor="">Column limit: </label>
+            <RIEInput
+              className="limit"
+              value={this.state.limit}
+              change={this.handleInputChange}
+              propName="limit"
+            />
           </div>
-        )}
-      </Droppable>
-    </div>
-  );
-};
+
+          {this.props.first ? null : (
+            <button
+              onClick={() =>
+                this.props.columnMoveHandler("left", this.props.ind)
+              }
+              className="icon-button"
+            >
+              <img className="icon" src={ArrowLeft} alt="" />
+            </button>
+          )}
+          <p className="column-title">{title}</p>
+          <p>
+            {this.props.limit
+              ? `${this.props.tickets.length}/${this.props.limit}`
+              : null}
+          </p>
+
+          <DeleteColumnButton
+            sourceColumnId={id}
+            columnHasTickets={tickets.length > 0 ? true : false}
+            getBoardData={this.props.getBoardData}
+            boardId={boardId}
+            columns={columns}
+          />
+          {this.props.last ? null : (
+            <button
+              onClick={() =>
+                this.props.columnMoveHandler("right", this.props.ind)
+              }
+              className="icon-button"
+            >
+              <img className="icon" src={ArrowRight} alt="" />
+            </button>
+          )}
+        </div>
+
+        {/* this is necessary for the drag and drop to work so that after dnd we know which is the source and destination column. KEY IS IMPORTANT! DON'T DELETE, MOVE OR WHATEVER! OTHERWISE DRAG AND FUCKING DROP WILL NOT FUCKING MOVE! */}
+        <Droppable key={id} droppableId={title}>
+          {(provided, snapshot) => (
+            <div
+              className="column-container"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <div className="column-tickets">
+                {ticketsList}
+                {/* the placeholder is there so that the space from which a card is dragged  doesn't collapse */}
+                {provided.placeholder}
+              </div>
+            </div>
+          )}
+        </Droppable>
+      </div>
+    );
+  }
+}
 
 export default Column;
