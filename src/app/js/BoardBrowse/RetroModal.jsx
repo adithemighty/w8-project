8fa7ac20165de170fca584d776dc2760c801fb7b
adiyathereh <ad.kydy@gmail.com>
2018-09-27 23:25:00 +0200

    finish retro modal

--- src/app/js/BoardBrowse/RetroModal.jsx
+++ src/app/js/BoardBrowse/RetroModal.jsx
@@ -5,17 +5,29 @@ const RetroModal = props => {
   return (
     <div className="modal">
       <div className="modal-text ">
-        <p>Here be retro modal</p>
-        <div className="marg-top-md action-btns">
-          <a
-            target="_blank"
-            href="https://retromat.org/en"
-            className="btn-md text-btn confirm "
-          >
-            Show me a retro format!
+        <p>Retrospectives are an important tool to improve team performances</p>
+        <br />
+
+        <p>
+          There are hundreds of different formats <br />
+          <a target="_blank" href="https://retromat.org/en">
+            Click here to get a random retrospective
           </a>
+        </p>
+
+        <div className="marg-top-md action-btns">
           <button
-            className="btn-md text-btn neutral marg-left-md"
+            className="btn-lg text-btn confirm "
+            onClick={() => {
+              props.openModal("retro");
+
+              props.history.push(`/b/${props.match.params.id}/t/new`);
+            }}
+          >
+            Create new action item
+          </button>
+          <button
+            className="btn-lg text-btn neutral marg-left-md"
             onClick={() => {
               //   const type = this.state.edit ? "edit" : "add";
               props.openModal("retro");
