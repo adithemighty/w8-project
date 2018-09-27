bf5b127fae595716475e0f886c8a612f202aee1b
adiyathereh <ad.kydy@gmail.com>
2018-09-27 15:34:53 +0200

    added some styling

--- src/app/js/Card/CardShowAndCreate.jsx
+++ src/app/js/Card/CardShowAndCreate.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
-import { Link } from "react-router-dom";
+import EditIcon from "../../assets/edit.svg";
 
 import api from "../utils/api";
-
-import { RIEInput, RIETextArea, RIESelect } from "riek";
 
 class CardShowAndCreate extends Component {
   constructor(props) {
@@ -17,13 +15,12 @@ class CardShowAndCreate extends Component {
       id: null,
       description: "",
       estimation: 0,
-      ticketType: ""
-      // ticketTypeFieldValue: { id: "2", text: "User Story" },
-      // ticketTypeOptions: [
-      //   { id: "1", text: "Bug" },
-      //   { id: "2", text: "User Story" },
-      //   { id: "3", text: "Action Item" }
-      // ]
+      ticketType: "",
+      titleInputFieldShown: false,
+      estimationInputFieldShown: false,
+      ticketTypeInputFieldShown: false,
+      descriptionTypeInputFieldShown: false,
+      oneFieldShown: false
     };
   }
 
@@ -36,13 +33,132 @@ class CardShowAndCreate extends Component {
         <div className="card-details">
           {/* Here come the editable fields made with RIE */}
           <div className="editable-fields">
-            <label>Title</label>
-            <input
-              type="text"
-              onChange={e => this.handleInputChange(e.target.value)}
-              value={this.state.title}
-            />
+            <div className="row row-one">
+              <label htmlFor="title">Title</label>
+              {this.state.titleInputFieldShown ||
+              !this.state.title ||
+              this.state.new ? (
+                <input
+                  className="active-input input"
+                  type="text"
+                  id="title"
+                  onChange={e =>
+                    this.handleInputChange("title", e.target.value)
+                  }
+                  value={this.state.title}
+                />
+              ) : (
+                <p
+                  className="input"
+                  onClick={() => {
+                    this.toggleInputField("title");
+                  }}
+                >
+                  {this.state.title}
+                </p>
+              )}
+            </div>
 
+            <div className="row row-two">
+              <div className="row-column">
+                <label htmlFor="ticketType">Issue type:</label>
+
+                {this.state.ticketTypeInputFieldShown ||
+                !this.state.ticketType ||
+                this.state.new ? (
+                  <select
+                    className="active-input select"
+                    onChange={e =>
+                      this.handleInputChange("ticketType", e.target.value)
+                    }
+                    name="ticketType"
+                    id="ticketType"
+                  >
+                    <option
+                      className="active-input option"
+                      value=""
+                      selected
+                      disabled
+                      hidden
+                    >
+                      {this.state.ticketType
+                        ? this.state.ticketType
+                        : "User Story"}
+                    </option>
+                    <option className="active-input option" value="User Story">
+                      User Story
+                    </option>
+                    <option className="active-input option" value="Bug">
+                      Bug
+                    </option>
+                    <option className="active-input option" value="Action Item">
+                      Action Item
+                    </option>
+                  </select>
+                ) : (
+                  <p
+                    onClick={() => {
+                      this.toggleInputField("ticketType");
+                    }}
+                  >
+                    {this.state.ticketType}
+                  </p>
+                )}
+              </div>
+
+              <div className="row-column">
+                <label htmlFor="estimation">Estimation</label>
+
+                {this.state.estimationInputFieldShown ||
+                !this.state.estimation ||
+                this.state.new ? (
+                  <input
+                    className="active-input input"
+                    onChange={e =>
+                      this.handleInputChange("estimation", e.target.value)
+                    }
+                    type="number"
+                    min="0"
+                    id="estimation"
+                    value={this.state.estimation}
+                  />
+                ) : (
+                  <p
+                    onClick={() => {
+                      this.toggleInputField("estimation");
+                    }}
+                  >
+                    {this.state.estimation}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="row row-three">
+              <label htmlFor="description">Description</label>
+
+              {this.state.descriptionInputFieldShown ||
+              !this.state.description ||
+              this.state.new ? (
+                <textarea
+                  className="active-input textarea"
+                  onChange={e =>
+                    this.handleInputChange("description", e.target.value)
+                  }
+                  value={this.state.description}
+                  id="description"
+                />
+              ) : (
+                <p
+                  className=" textarea"
+                  onClick={() => {
+                    this.toggleInputField("description");
+                  }}
+                >
+                  {this.state.description}
+                </p>
+              )}
+            </div>
             {/* action handlers for cancel and submit */}
 
             <div className="action-btns">
@@ -67,8 +183,14 @@ class CardShowAndCreate extends Component {
     );
   }
 
+  toggleInputField = type => {
+    const field = `${type}InputFieldShown`;
+    this.setState((prevState, props) => {
+      return { [field]: !prevState[field], oneFieldShown: true };
+    });
+  };
+
   componentDidMount = () => {
-    console.log("hallo");
     if (this.props.match.path.indexOf("new") > 0) {
       this.setState((prevState, props) => {
         return { new: true, title: "" };
@@ -94,8 +216,6 @@ class CardShowAndCreate extends Component {
         estimation
       };
 
-      //   make sure that all states in the field have some value because RIE cannot handle undefined as value
-
       this.setState((prevState, props) => {
         return newState;
       });
@@ -108,22 +228,33 @@ class CardShowAndCreate extends Component {
     }
   };
 
-  handleInputChange = value => {
+  handleInputChange = (key, value) => {
     console.log(value);
     this.setState((prevState, props) => {
-      return { title: value };
+      return { [key]: value };
     });
   };
 
   handleInputSubmit = () => {
-    const { title } = this.state;
+    const { title, ticketType, estimation, description } = this.state;
 
-    // const submitableFields = { title, description, estimation, blocker };
-    // submitableFields["boardId"] = this.props.match.params.boardId;
+    const submitableFields = {};
+    if (title) {
+      submitableFields["title"] = title;
+    }
+    if (ticketType) {
+      submitableFields["ticketType"] = ticketType;
+    }
+    if (estimation) {
+      submitableFields["estimation"] = Number(estimation);
+    }
+    if (description) {
+      submitableFields["description"] = description;
+    }
+    console.log(submitableFields);
 
-    // submitableFields["ticketType"] = this.state.ticketType.text;
     if (!this.state.new) {
-      api.post(`/api/t/update/${this.state.id}`, { title }).then(() => {
+      api.post(`/api/t/update/${this.state.id}`, submitableFields).then(() => {
         this.props.ticketDetailViewOpenHandler();
         this.props.getBoardData();
         this.props.history.push(`/b/${this.props.match.params.boardId}`);
