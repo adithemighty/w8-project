75c54bb4c57e15440596083de2e498298cb244c5
Adiya Mohr <adiya.mohr@signavio.com>
2018-10-01 18:51:12 +0200

    implement tooltip for create new issue

---
+++ src/app/js/Component/Tooltip.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Tooltip extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+  }
+  handleMouseIn = () => {
+    this.setState({ hover: true });
+  };
+
+  handleMouseOut = () => {
+    this.setState({ hover: false });
+  };
+
+  render() {
+    const { element, tooltipText } = this.props;
+
+    const visibility = this.state.hover
+      ? "tooltip-visible tooltip-size-md"
+      : "tooltip-invisible";
+
+    return (
+      <div className="tooltip-parent">
+        <div onMouseOver={this.handleMouseIn} onMouseOut={this.handleMouseOut}>
+          {element}
+        </div>
+
+        <div>
+          <div className={visibility}>{tooltipText}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tooltip;
